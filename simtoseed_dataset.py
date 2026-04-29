1b39fc57380d53648423d40cabb22b81a22dc847
HotshotGoku <kinshuk.sahu@duke.edu>
2026-04-29 15:53:30 -0400

    Dataset for sim to seed

--- simtoseed_dataset.py
+++ simtoseed_dataset.py
@@ -9,11 +9,13 @@ source_folder= SIM_FOLDER_SEEDTOSIM
 target_folder= SEED_FOLDER_SEEDTOSIM
 
 class MyDataset(Dataset):
-    def __init__(self, source_folder:str=SIM_FOLDER_SEEDTOSIM, target_folder:str=SEED_FOLDER_SEEDTOSIM, start:int=0,end:int=30000):
+    def __init__(self, source_folder:str=SIM_FOLDER_SEEDTOSIM, target_folder:str=SEED_FOLDER_SEEDTOSIM, start:int=0,end:int=30000, use_vae=False, preprocess_seed_output= True):
         # Store full paths to avoid repeated path joining
         # choose the first N files for consistency
+        self.use_vae= use_vae
         self.source_files = sorted([os.path.join(source_folder, f) for f in os.listdir(source_folder)])[start:end]
         self.target_files = sorted([os.path.join(target_folder, f) for f in os.listdir(target_folder)])[start:end]
+        self.preprocess_seed_output = preprocess_seed_output
         
         # Sanity check
         assert len(self.source_files) == len(self.target_files), \
@@ -29,7 +31,13 @@ class MyDataset(Dataset):
 
         # Load and preprocess the data 
         source = preprocess_simulation_graybackground(source_path)
-        target = preprocess_seed(target_path)
+        if self.use_vae== False:
+            target = preprocess_seed(target_path)
+        elif self.use_vae== True and self.preprocess_seed_output== True:
+            target = preprocess_seed(target_path, left_crop=2, right_crop=3, img_length=32, img_width=32)  # VAE latent size
+        else:
+            target = preprocess_seed(target_path, img_length=32, img_width=32)  # no cropping if already preprocessed to 32x32
+
 
         # Normalize source and target images to [0,1] for Logits loss 
 
